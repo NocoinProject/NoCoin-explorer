ad05bdf8cbc8f9f69c437a54d87cac552544646a
NocoinProject <41588607+NocoinProject@users.noreply.github.com>
2019-04-11 19:47:07 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,21 @@
+const ExplorerConfig = {
+  apiBaseUrl: '87.177.59.70:11898',
+  poolApiUrl: null
+  poolListUrl: null
+  ticker: 'NOC',
+  maxSupply: 2100000000000000,
+  emissionSpeed: 23,
+  decimalPoints: 0,
+  blockTargetTime: 20,
+  startYear: 2019,
+  genesisTimestamp: 1554733544,
+  emissionCurveInterval: 20000,
+  emissionCurveDataPoints: 15000,
+  addressPrefix: 9602,
+  forkHeights: [
+    788400,  // 0
+    1576800,  // 1
+    2365200,  // 2
+    3153600,  // 3
+  ]
+}
